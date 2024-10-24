72101f9f66717764c725acdab1e329dc6d41819f
AlaaMousa05 <loloadnan.2024@gmail.com>
2024-10-24 14:48:12 -0700

    updateNav

--- main.js
+++ main.js
@@ -17,7 +17,15 @@ if (navClose) {
       nav.classList.remove("show-menu");
    });
 }
-
+// scroll page
+window.addEventListener("scroll", function() {
+   var header = document.querySelector(".header");
+   if (window.scrollY > 50) {
+     header.classList.add("scrolled");
+   } else {
+     header.classList.remove("scrolled");
+   }
+ });
 /* --------------- movies Container part -------------------- */
 
 fetchData();
